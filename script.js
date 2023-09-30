86cd216d7ff5a4ba70e878ac62049c6592425596
ESAKKIMUTHU S <141995442+sesakkimuthu@users.noreply.github.com>
2023-09-30 11:55:47 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,70 @@
+
+var mainImage = document.getElementById('MainImage')
+
+var smallImg = document.getElementsByClassName('small-img')
+
+
+
+smallImg[0].onclick = function(){
+  mainImage.src = smallImg[0].src
+}
+
+smallImg[1].onclick = function(){
+  mainImage.src = smallImg[1].src
+}
+
+
+smallImg[2].onclick = function(){
+  mainImage.src = smallImg[2].src
+}
+
+smallImg[3].onclick = function(){
+  mainImage.src = smallImg[3].src
+}
+
+
+var mainImage1 = document.getElementById('MainImage1')
+
+var smallImg1 = document.getElementsByClassName('small-img1')
+
+
+
+smallImg1[0].onclick =  function(){
+  mainImage1.src = smallImg1[0].src
+}
+
+smallImg1[1].onclick = function(){
+  mainImage1.src = smallImg1[1].src
+}
+
+smallImg1[2].onclick =  function(){
+  mainImage1.src = smallImg1[2].src
+}
+
+smallImg1[3].onclick = function(){
+  mainImage1.src = smallImg1[3].src
+} 
+
+
+
+var mainImage2 = document.getElementById('MainImage2')
+
+var smallImg2 = document.getElementsByClassName('small-img2')
+
+
+
+smallImg2[0].onclick =  function(){
+  mainImage2.src = smallImg2[0].src
+}
+
+smallImg2[1].onclick = function(){
+  mainImage2.src = smallImg2[1].src
+}
+
+smallImg2[2].onclick =  function(){
+  mainImage2.src = smallImg2[2].src
+}
+
+smallImg2[3].onclick = function(){
+  mainImage2.src = smallImg2[3].src
+}
